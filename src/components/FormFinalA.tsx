052cca80389b96bc9d774f6bd007be6cd1ff647f
michal.c <michal.ciezki@binarapps.com>
2024-07-15 15:23:56 +0200

    try to change components order

--- src/components/FormFinalA.tsx
+++ src/components/FormFinalA.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 // import FormInput from './FormInput';
 import SuccessCustomerInsightsForm from './customer-insights-form/SuccessCustomerInsightsForm';
 
@@ -8,7 +8,35 @@ import SuccessCustomerInsightsForm from './customer-insights-form/SuccessCustome
 // }
 
 const FormFinalA: React.FC = () => {
-  return <SuccessCustomerInsightsForm />;
+  const [isScriptLoaded, setIsScriptLoaded] = useState(false);
+
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src =
+      'https://cxppusa1formui01cdnsa01-endpoint.azureedge.net/eur/FormLoader/FormLoader.bundle.js';
+    script.async = true;
+
+    script.onload = () => {
+      console.log('External script loaded successfully - it should work');
+      setIsScriptLoaded(true);
+    };
+
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
+  return (
+    <div>
+      {isScriptLoaded ? (
+        <SuccessCustomerInsightsForm />
+      ) : (
+        <p>Skrypt się nie wczytał</p>
+      )}
+    </div>
+  );
 };
 
 export default FormFinalA;
--- src/components/FormFinalB.tsx
+++ src/components/FormFinalB.tsx
@@ -1,8 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import FallbackCustomerInsightsForm from './customer-insights-form/FallbackCustomerInsightsForm';
 
 const FormFinalB: React.FC = () => {
-  return <FallbackCustomerInsightsForm />;
+  const [isScriptLoaded, setIsScriptLoaded] = useState(false);
+
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src =
+      'https://cxppusa1formui01cdnsa01-endpoint.azureedge.net/eur/FormLoader/FormLoader.bundle.js';
+    script.async = true;
+
+    script.onload = () => {
+      console.log('External script loaded successfully - it should work');
+      setIsScriptLoaded(true);
+    };
+
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
+  return (
+    <div>
+      {isScriptLoaded ? (
+        <FallbackCustomerInsightsForm />
+      ) : (
+        <p>Skrypt się nie wczytał</p>
+      )}
+    </div>
+  );
 };
 
 export default FormFinalB;
