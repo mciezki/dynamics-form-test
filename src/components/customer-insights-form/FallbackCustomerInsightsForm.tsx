61ad0e24cbf00610fbc84fbaa693f9ad082dd0a9
michal.c <michal.ciezki@binarapps.com>
2024-07-15 17:56:32 +0200

    Next fixes

--- src/components/customer-insights-form/FallbackCustomerInsightsForm.tsx
+++ src/components/customer-insights-form/FallbackCustomerInsightsForm.tsx
@@ -1,23 +1,23 @@
 // src/components/CustomerInsightsForm.tsx
-import React, { useEffect } from 'react';
+import React, { useLayoutEffect } from 'react';
 
 const FallbackCustomerInsightsForm: React.FC = () => {
-  // useEffect(() => {
-  //   const script = document.createElement('script');
-  //   script.src =
-  //     'https://cxppusa1formui01cdnsa01-endpoint.azureedge.net/eur/FormLoader/FormLoader.bundle.js';
-  //   script.async = true;
+  useLayoutEffect(() => {
+    const script = document.createElement('script');
+    script.src =
+      'https://cxppusa1formui01cdnsa01-endpoint.azureedge.net/eur/FormLoader/FormLoader.bundle.js';
+    script.async = true;
 
-  //   script.onload = () => {
-  //     console.log('External script loaded successfully');
-  //   };
+    script.onload = () => {
+      console.log('External script loaded successfully - it should work');
+    };
 
-  //   document.body.appendChild(script);
+    document.body.appendChild(script);
 
-  //   return () => {
-  //     document.body.removeChild(script);
-  //   };
-  // }, []);
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
 
   return (
     <div
