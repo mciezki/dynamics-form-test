e9ff751ff47fb8c30b318b7e5db5bb58d4d1877d
michal.c <michal.ciezki@binarapps.com>
2024-07-15 16:48:51 +0200

    add new form for test

--- src/components/customer-insights-form/SuccessCustomerInsightsForm.tsx
+++ src/components/customer-insights-form/SuccessCustomerInsightsForm.tsx
@@ -21,10 +21,15 @@ const SuccessCustomerInsightsForm: React.FC = () => {
 
   return (
     <div
-      data-form-id='5c00205c-3240-ef11-a316-7c1e52209e09'
+      data-form-id='456a6306-b942-ef11-a316-7c1e52209e09'
       data-form-api-url='https://public-eur.mkt.dynamics.com/api/v1.0/orgs/90486ce6-c03e-ef11-8e4b-002248a38b79/landingpageforms'
-      data-cached-form-url='https://assets-eur.mkt.dynamics.com/90486ce6-c03e-ef11-8e4b-002248a38b79/digitalassets/forms/5c00205c-3240-ef11-a316-7c1e52209e09'
+      data-cached-form-url='https://assets-eur.mkt.dynamics.com/90486ce6-c03e-ef11-8e4b-002248a38b79/digitalassets/forms/456a6306-b942-ef11-a316-7c1e52209e09'
     ></div>
+    // <div
+    //   data-form-id='5c00205c-3240-ef11-a316-7c1e52209e09'
+    //   data-form-api-url='https://public-eur.mkt.dynamics.com/api/v1.0/orgs/90486ce6-c03e-ef11-8e4b-002248a38b79/landingpageforms'
+    //   data-cached-form-url='https://assets-eur.mkt.dynamics.com/90486ce6-c03e-ef11-8e4b-002248a38b79/digitalassets/forms/5c00205c-3240-ef11-a316-7c1e52209e09'
+    // ></div>
   );
 };
 
